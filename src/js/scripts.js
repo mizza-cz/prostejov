9a3b508bbd0535580a24c1862978740bfa7a8627
mizza-cz <mizza.cz666@gmail.com>
2023-02-16 19:33:14 +0100

    add players

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,6 +5,7 @@
 //=include ../../node_modules/slick-carousel/slick/slick.js
 //=include ../../node_modules/lightgallery/lightgallery.umd.js
 //=include ../../node_modules/lightgallery/lightgallery.min.js
+//=include ../../node_modules/select2/dist/js/select2.min.js
 document.addEventListener("DOMContentLoaded", function () {
   //=include ../components/**/*.js
   //=include custom/*.js
